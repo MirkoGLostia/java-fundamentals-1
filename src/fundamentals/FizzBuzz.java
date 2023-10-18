6eabdda9659609207cb453ef56bc9dff0f84e81e
MirkoGLostia <giggi.torre@mail.com>
2023-10-18 14:34:39 +0200

    aggiunta commenti a fizzbuzz

--- src/fundamentals/FizzBuzz.java
+++ src/fundamentals/FizzBuzz.java
@@ -3,14 +3,19 @@ package fundamentals;
 public class FizzBuzz {
 
     public static void main(String[] args) {
+        // ciclo per stampare i numeri da 1 a 100
         for (int i = 1; i <= 100; i++) {
             if ((i % 3 == 0 ) && (i % 5 == 0)){
+                // condizione and, se messa dopo sarà sempre falsa
                 System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
+                // condizione se il numero è divisibile per 5
                 System.out.println("Buzz");
             } else if ((i % 3 == 0 )) {
+                // condizione se il numero è divisibile per 3
                 System.out.println("Fizz");
             }else {
+                // qualsiasi altro numero non corrisponda a nessuna condizione
                 System.out.println(i);
             }
         }
