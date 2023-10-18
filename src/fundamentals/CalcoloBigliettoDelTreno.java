516f7fe4f3fc3f9f95e06dc7afb0d531f32a21b0
MirkoGLostia <giggi.torre@mail.com>
2023-10-18 15:35:34 +0200

    richiesta dati utente e prezzo biglietto

--- src/fundamentals/CalcoloBigliettoDelTreno.java
+++ src/fundamentals/CalcoloBigliettoDelTreno.java
@@ -1,7 +1,40 @@
 package fundamentals;
 
+import java.util.Scanner;
+
 public class CalcoloBigliettoDelTreno {
     public static void main(String[] args) {
 
+        // inizializzazione scanner
+        Scanner inputUtente = new Scanner(System.in);
+
+        // richiesta dati all'utente
+        System.out.println("How far will you go?");
+        System.out.print("Km ");
+        int distance = inputUtente.nextInt();
+
+
+        System.out.println("How old are you?");
+        int userAge = inputUtente.nextInt();
+
+
+        // calcolo costo biglietto
+        double price = distance * 0.21;
+
+        double finalPrice = price;
+
+        if (userAge < 18){
+            finalPrice = price - (price/100*20);
+        } else if (userAge > 65) {
+            finalPrice = price - (price/100*40);
+        }
+
+
+        System.out.print("ticket price: ");
+        System.out.println(finalPrice + " euro");
+
+
+        // chiusura scanner
+        inputUtente.close();
     }
 }
