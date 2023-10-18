ad1a9989b028561d5bac6f8dc1849a80f59e86e8
MirkoGLostia <giggi.torre@mail.com>
2023-10-18 15:05:14 +0200

    check invitati indipendente dalle maiuscole

--- src/fundamentals/FestaACasaFerragnez.java
+++ src/fundamentals/FestaACasaFerragnez.java
@@ -19,7 +19,7 @@ public class FestaACasaFerragnez {
 
         // confronto nome utente nella lista con un ciclo
         for (int i = 0; i < listaInvitati.length; i++) {
-            if (nomeUtente.equals(listaInvitati[i])){
+            if (nomeUtente.toLowerCase().equals(listaInvitati[i].toLowerCase())){
                 checkList = true;
                 break;
             }
