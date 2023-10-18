49792d5efb67444efc247715c9ad37c3d7fbdcaa
MirkoGLostia <giggi.torre@mail.com>
2023-10-18 14:39:21 +0200

    array invitati in ferragnez

--- src/fundamentals/FestaACasaFerragnez.java
+++ src/fundamentals/FestaACasaFerragnez.java
@@ -3,5 +3,9 @@ package fundamentals;
 public class FestaACasaFerragnez {
     public static void main(String[] args) {
 
+        // creazione array degli invitati
+        String[] listaInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+
     }
 }
